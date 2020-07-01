b9fe54d71476added2e0c0665cf48b6ee94737ff
Clara <clara.wang.94@gmail.com>
2020-07-01 13:29:43 -0400

    add cleaning

--- analyze_mf.py
+++ analyze_mf.py
@@ -4,3 +4,23 @@ import re
 
 with open('C:/Users/clara/Documents/china_fm/tutorial/mf_press.json', 'rt', encoding='utf-8') as f:
     data = json.load(f, encoding="utf-8")
+
+def clean_html_tags(remarks):
+    clean = re.sub('(?<=<).*?(?=>)', '', remarks)
+    clean = re.sub('<', '', clean)
+    clean = re.sub('>', '', clean)
+    return(clean)
+
+for i in range(0, len(data)):
+    print(i)
+    entry = data[i]
+    title = entry['title']
+    line = entry['text']
+    cleanstring = ''
+    for x in line:
+        clean = clean_html_tags(x)
+        cleanstring = cleanstring + clean
+
+    print(title)
+    if 'Q:' not in cleanstring or 'A:' not in cleanstring:
+        print(cleanstring)
