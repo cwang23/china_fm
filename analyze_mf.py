6d96968e6ae3b7b663407aacff78bdef36d54b9e
Clara <clara.wang.94@gmail.com>
2020-07-01 19:43:24 -0400

    ignore png

--- analyze_mf.py
+++ analyze_mf.py
@@ -15,18 +15,18 @@ def clean_html_tags(remarks):
 clean_output = list()
 
 for i in range(0, len(data)):
-    print(i)
+    # print(i)
     entry = data[i]
     title = entry['title'][0]
     line = entry['text']
     clean_url = entry['url']
 
     # clean title information
-    clean_spox = re.findall("(?<=^Foreign Ministry Spokesperson )(.*)(?=')", title)[0]
+    clean_spox = re.findall("(?<=^Foreign Ministry Spokesperson )(.+?)(?=')", title)[0]
     topicdate = re.findall("(?<= on )(.*)(?=)", title)
 
-    clean_date = "No Date or Topic"
-    clean_topic = "No Date or Topic"
+    clean_date = "No Date"
+    clean_topic = "No Topic"
     if len(topicdate) > 0:
         if "20" in topicdate[0]:
             clean_date = topicdate[0]
@@ -43,39 +43,69 @@ for i in range(0, len(data)):
     # clean the remarks
     clean_remarks = []
     clean_order = []
+    clean_contenttype = []
     cleanstring = ''
 
     order_start = 1
     for x in line:
         clean = clean_html_tags(x)
+        # print(clean)
 
         # check if there's a Q: or CNN: at the beginning of line
         q_or_a = bool(re.match("^[A-Za-z ]{1,50}:", clean))
+        # print(f'q_or_a: {q_or_a}')
+        answer_a = bool(re.match("^A:", clean))
+        # print(f'answer_a: {answer_a}')
+        answer_name = clean_spox in clean
+
+
+        answer_block = answer_a or answer_name
+        # print(f'answer_block: {answer_block}')
+        question_block = q_or_a and not answer_block
+        # print(f'question_block: {question_block}')
+
+        if answer_block:
+            blocktype = "A"
+        elif question_block:
+            blocktype = "Q"
+        else:
+            blocktype = "None"
 
         # if just beginning, set cleaned string as start
         if cleanstring == '':
             cleanstring = clean
+            cleantype = blocktype
         # if it's a question or answer, set the start of new clean string
         # to this beginning question or answer
         elif q_or_a:
             # add the cleaned info to list
             clean_remarks.append(cleanstring)
             clean_order.append(order_start)
+            clean_contenttype.append(cleantype)
+
             # reset values
             order_start += 1
             cleanstring = clean
+            cleantype = blocktype
         # if it's not the beginning of a section, add it to the string
         else:
             cleanstring = cleanstring + ' ' + clean
 
+    # add the last paragraph to the string
+    clean_remarks.append(cleanstring)
+    clean_order.append(order_start)
+    clean_contenttype.append(cleantype)
+
     # make dictionary to store cleaned info
     out = {
+        "title": title,
         "spox": clean_spox,
         "date": clean_date,
         "topic": clean_topic,
         "type": clean_type,
         "content": clean_remarks,
         "content_order": clean_order,
+        "content_type": clean_contenttype,
         'url': clean_url
     }
     clean_output.append(out)
@@ -83,5 +113,5 @@ for i in range(0, len(data)):
 
 full_clean = pd.DataFrame(clean_output)
 
-expanded_full_clean = full_clean.set_index(['date', 'spox', 'topic', 'type', 'url']).apply(pd.Series.explode).reset_index()
+expanded_full_clean = full_clean.set_index(['title', 'date', 'spox', 'topic', 'type', 'url']).apply(pd.Series.explode).reset_index()
 expanded_full_clean.to_csv("C:/Users/clara/Documents/china_fm/clean_mf.csv")
